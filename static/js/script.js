3d3de3c5bcbed05a1ca30153f11707e670d10bbf
Gustavo Peretti <gustavoperetti11@gmail.com>
2024-07-09 11:37:04 -0300

    Adição de responsividade para navbar

--- static/js/script.js
+++ static/js/script.js
@@ -34,11 +34,40 @@ document.onscroll = () => {
     if (scrollY > document.documentElement.clientHeight) {
         document.querySelector('nav').style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
         document.querySelector('nav').style.boxShadow = 'rgba(0, 0, 0, 0.5)';
-        document.querySelectorAll('nav a').forEach(e => e.style.color = 'white');
+        document.querySelectorAll('nav a, nav span').forEach(e => e.style.color = 'white');
         return;
     }
     
     document.querySelector('nav').style.backgroundColor = 'transparent';
     document.querySelector('nav').style.boxShadow = '';
-    document.querySelectorAll('nav a').forEach(e => e.style.color = 'black');
+    document.querySelectorAll('nav a, nav span').forEach(e => e.style.color = 'black');
 };
+
+window.onresize = () => {
+    if (window.innerWidth >= 560) {
+        document.querySelector('#hamburguer').style.display = 'none';
+        document.querySelector('nav ul').style.flexDirection = 'row';
+        document.querySelector('nav ul').style.height = 'auto';
+        document.querySelector('nav ul').style.padding = '1em';
+        return;
+    }
+
+    document.querySelector('#hamburguer').style.display = 'inline';
+    document.querySelector('nav ul').style.flexDirection = 'column';
+    document.querySelector('nav').style.paddingTop = '1em';
+    document.querySelector('nav ul').style.padding = '0';
+    document.querySelector('nav ul').style.height = '0';
+}
+
+window.onload = () => {
+    document.onscroll();
+    window.onresize();
+};
+
+document.querySelector('#hamburguer').addEventListener('click', () => {
+    if (document.querySelector('nav ul').style.height == '10em') {
+        document.querySelector('nav ul').style.height = '0';
+        return;
+    }
+    document.querySelector('nav ul').style.height = '10em';
+});
